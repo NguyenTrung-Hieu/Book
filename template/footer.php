aa594e54cc06a8af756ce4a77b8ea3acd04333e6
NguyenTrung-Hieu <trunghieu19990919@gmail.com>
2021-04-26 13:01:53 +0700

    add

---
+++ template/footer.php
@@ -0,0 +1,20 @@
+      	<hr>
+
+      	<footer>
+        	<div class="text-muted pull-left">
+            	<p>Nguyễn Trung Hiếu - Nguyễn Hoàng Quân - Nguyễn Thành Công - Team HQC - HUTECH</p>
+        	</div>
+        	<div class="text-muted pull-right">
+          		<a href="admin.php">Admin Login</a>
+        	</div>
+      	</footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript" src="./bootstrap/js/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
